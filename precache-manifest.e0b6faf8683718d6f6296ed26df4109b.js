346104aa7b93ad74a13c105062857294a84d418b
Fabien Petitjean <f.petitjean@lombardodier.com>
2019-03-21 16:50:17 +0100

    First release.

---
+++ precache-manifest.e0b6faf8683718d6f6296ed26df4109b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98554640f1ac2b0bda5d",
+    "url": "/react-test/static/js/runtime~main.1043f503.js"
+  },
+  {
+    "revision": "6a1e1a58325db872e2e0",
+    "url": "/react-test/static/js/main.3c0cc0d0.chunk.js"
+  },
+  {
+    "revision": "3ea3eeaed7f49dd2d42c",
+    "url": "/react-test/static/js/2.92779abb.chunk.js"
+  },
+  {
+    "revision": "6a1e1a58325db872e2e0",
+    "url": "/react-test/static/css/main.1ffe44c6.chunk.css"
+  },
+  {
+    "revision": "96c9c9006ab8c7bc33a7642d1394df60",
+    "url": "/react-test/index.html"
+  }
+];
